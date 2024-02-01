423137f7312d529675663605fa20948e2f68e0c4
Oleksandr Melnychuk <melnychuk95@gmail.com>
2024-02-01 17:39:01 +0000

    stage 1

--- hardhat-smartlotery/contracts/Raffle.sol
+++ hardhat-smartlotery/contracts/Raffle.sol
@@ -8,22 +8,30 @@
 
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
-
+import "node_modules/@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
 error Raffle_NotEnoughtETH();
 
-contract Raffle{
+contract Raffle is VRFConsumerBaseV2{
     uint256 private immutable i_entrancefee;
     address payable[] private s_players;
-    constructor(uint256 entrancefee){
+    event RaffleEnster(address indexed player);
+    constructor(address vrfCoordinatorV2 ,uint256 entrancefee) VRFConsumerBaseV2(vrfCoordinatorV2){
         i_entrancefee = entrancefee;
     }
+
     function enterRaffle() public payable{
         if(msg.value < i_entrancefee){
             revert Raffle_NotEnoughtETH();
         }
         s_players.push(payable(msg.sender));
         //events
-        
+        emit RaffleEnster(msg.sender);
+    }
+    function reqpickRandomNumb()external{
+
+    }
+    function fulfillRandomWords(uint256 reqID, uint256[] memory randomWords) internal override{
+
     }
     function getFee() public view returns(uint256){
         return i_entrancefee;
